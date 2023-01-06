127ca375d6dcaa5651a4e2a5fcf2ebd8282f7cc0
Vanshika Juneja <90781222+Vanshi10@users.noreply.github.com>
2023-01-07 01:25:25 +0530

    Create message.js

---
+++ message.js
@@ -0,0 +1,129 @@
+export default [
+  {
+    heading: "Buy second-hand goods!",
+    msg: "As a student, buying second-hand goods is a great way to save money because prices are usually much lower than for brand-new items."
+  },
+  {
+    heading: "Search for free entertainment.",
+    msg: "Most of the students think having fun means spending lots of money on movies, parties, clubs, theme parks, or other local attractions. Remember many clubs, cinemas and theaters will offer discounts for students – if it’s not clearly advertised, just ask. The university will have plenty of cheap entertainment, and joining student clubs will often mean you get to benefit from even more discounts."
+  },
+  {
+    heading: "Don't spend on what is not needed",
+    msg: "Teenagers are impulsive and as soon as they get pocket money, they tend to spend it on games or food right away. But, they have to remember that what they want isn't always what they need. This will help them make better decisions before spending money. They can also keep things such as a piggy bank and a saving jar to save money."
+
+  },
+  {
+    heading: "Smart money management apps",
+    msg: "We are in a digital age where children as young as five know how to use a smartphone. Teenagers can make good use of it. There are many apps on smart money management which allow the user to manage their expenses, track them, and also set a budget so that they don't go overboard."
+  },
+  {
+    heading: "What is Financial Literacy? ",
+    msg: "First, you have to understand the meaning of Financial Literacy. Financial literacy is the understanding of key financial concepts and skills. It’s not just about knowing the information, but about successfully implementing it into your own life. Sometimes college students face unique economic challenges. They’re at an age where adopting basic financial skills and knowledge can profoundly impact their entire adulthood. There are some basic principles of Financial Literacy:"
+  },
+  {
+    heading: "1.	Earn ",
+    msg: "One critical component of financial literacy is the ability to earn money. Students can earn money by doing a part-time job, becoming a copywriter, starting their own YouTube Channel, starting Online Tutoring, and doing social marketing."
+  },
+  {
+    heading: "2.	Save and Invest ",
+    msg: "Saving is one of the most important ways to prepare for your financial future. You might be wondering how to get started saving. here is your answer. First track your saving and investment accounts, you can build emergency savings, invest for future expenses, Spend Less on Dining Out and Start investing in the stock market "
+  },
+  {
+    heading: "3.	Take advantage of government resources ",
+    msg: "Through the Fair and Accurate Credit Transactions Act of 2003, the federal government created its Financial Literacy and Education Commission. The purpose of this Commission is to help increase financial literacy among Americans. The Commission provides an educational website to help people learn about basic financial concepts. Agencies such as the Consumer Financial Protection Bureau and U.S. Securities and Exchange Commission also publish educational articles to help Americans learn about money."
+  },
+  {
+    heading: "4.	Take a finance course in college ",
+    msg: "You can take a Finance course in college or find online financial literacy resources. Living in the digital age, there is no shortage of online resources that can help you increase your financial literacy."
+  },
+  {
+    heading: "Personal Finance Basics- ",
+    heading: "Introduction to Bank Accounts",
+    msg: "Bank accounts are typically the first financial account that you’ll open and are necessary for major purchases and life events. Here’s a breakdown of which bank accounts you should consider and why they are step one in creating a stable financial future. "
+  },
+  {
+    heading: "Types of bank accounts you can open ",
+    msg: "There are three main types of bank account s that a person may want to open: "
+  },
+  {
+    heading: "1)	Savings account: ",
+    msg: "A savings account is an interest-bearing deposit account held at a bank or other financial institution. Savings accounts typically pay a low-interest rate, but their safety and reliability make them a sensible o ption for saving available cash for short-term needs."
+
+  },
+  {
+    heading: "2)	Checking account",
+    msg: "A checking account is also a deposit account at a bank or other financial institution that allows you to make deposits and withdrawals. Money can be deposited at banks and ATMs, through direct deposit, or through another type  of electronic transfer. Account holders can withdraw funds via banks and ATMs, by writing checks, or using debit cards linked to their accounts."
+  },
+  {
+    heading: "3)	High-yield savings account:",
+    msg: "A high-yield savings account is another type of savings account that usually pays a much higher rate of interest than a standard savings account. The tradeoff for earning more interest on your money is that high-yield accounts tend to require bigger initial deposits, larger minimum balances, and higher fees."
+  },
+  {
+    heading: "Introduction to Credit Cards ",
+    msg: "A credit card is a payment card issued to a cardholder to enable the cardholder to pay a merchant for goods and services based on the cardholder's accrued debt (i.e., promise to the card issuer to pay them for the amounts plus the other agreed charges). Having a credit card is a convenient way to pay for almost anything but it is important to remember that a credit card is a type of unsecured loan. An unsecured loan is a loan given without any security or collateral such as a house or a fixed deposit. Such loans are risky for banks and hence why credit card interest rates are usually very high."
+  },
+  {
+    heading: "What’s an emergency fund? ",
+    msg: "An emergency fund is a contingency fund that not only hel ps financially during the most difficult times but also prevents the derailment of your saving for long-term goals. An emergency fund cannot be built overnight but is done gradually. Set aside a particular amount every month in a different bank account. Soon it will grow into a considerable corpus that ywithdrawo have."
+  },
+  {
+    heading: "What is Financial Literacy?",
+    msg: "Financial literacy is the cognitive understanding of financial components and skills such as budgeting, investing, borrowing, taxation, and personal financial management. The absence of such skills is referred to as being financially illiterate. Being financially literate allows an individual to be better prepared for specific financial roadblocks, which in turn, decreases the chances of personal economic distress."
+  },
+  {
+    heading: "Fundamental Components of Financial Literacy",
+    msg: "Financial literacy consists of several financial components and skills that allow an individual to gain knowledge regarding the effective management of money and debt. Below are the fundamental components of financial literacy that should be learned."
+  },
+  {
+    heading: "1. Budgeting ",
+    msg: "Creating a solid budget is the first step in taking control of your financial life and making your money work for you. It can help you crush your outstanding debt, put money into savings, and feel more relaxed and less stressed out. By keeping track of your income and expenses each month, you can become a master of your money and meet your goals in a timely manner. Here are some ways that can help you to maintain your budget: Create a budgeting spreadsheet, Find your monthly income after taxes, List all of your fixed expenses, Write down your variable expenses, Compare your expenses to your income, Reduce your spending by purchasing less expensive items "
+  },
+  {
+    heading: "2.	Investing ",
+    msg: "To become financially literate, an individual must learn about key components in regard to investing. Some of the components that should be learned to ensure favorable investments are interest rates, price levels, diversification, risk mitigation, and indexes. The Best Investments For Young Adults are: can invest in Mutual Funds, Stock Market, Real Estate Investment Trusts(REITs), Policies, and Pay off Debt"
+  },
+
+  {
+    heading: "3.	Taxation ",
+    msg: "Gaining knowledge about the different forms of taxation and how they impact an individual’s net income is crucial for obtaining financial literacy. Awareness of the different income tax rates permits economic stability and increases financial performance through income management. The main types of taxes include-Income tax, Corporation tax, Sales tax/VAT, Excise duties, Production taxes, Environmental taxes, Stamp duty, Tarif, Inheritance tax,	Wealth tax, Capital gains tax, Poll Tax, Windfall taxes, Council taxes."
+  },
+  {
+    heading: "Benefits of Financial Literacy",
+    msg: "There are some assortment of benefits of being financially literate: Ability to make better financial decisions, Effective management of money and debt, Greater equipped to reach financial goals, Reduction of expenses through better regulation, Less financial stress and anxiety, Increase in ethical decision-making when selecting insurance, loans, investments, and using a credit card, Effective creation of a structured budget"
+  },
+  {
+    heading: "1.	Defining Life Goals: ",
+    msg: "Setting life goals allow people to have something to hope for. A reason to work towards a target objective. It helps when the goals are visualized with actual pictures of one’s dream house, business or any other thing. Some people tend to work hard more with a visualize dream. "
+  },
+  {
+    heading: "2.	Start Saving for Retirement:  ",
+    msg: "Tips to help you boost your retirement savings "
+  },
+  {
+    heading: "Automate your savings",
+    msg: "You've probably heard the phrase Pay Yourself First. Make your retirement contributions automatic each month and you'll have the opportunity to potentially grow your nest egg without having to think about it."
+
+  },
+  {
+    heading: "Set a goal",
+    msg: "Knowing how much you may need not only can help you better understand why you're saving, but also can make it more rewarding. Set benchmarks along the way and gain satisfaction as you pursue your retirement goal. "
+  },
+  {
+    heading: "Manage your debt",
+    msg: "Effective use of debit and credit cards can help you monitor your expenses and might also offer the advantages of reward programs. any Remember before you borrow, carefully analyze the impact any major purchases may have on your cash flow. As you pay off debt, direct the money you save into an emergency or retirement saving account."
+  },
+  {
+    heading: "3.	Invest in real estate ",
+    msg: "The benefits of investing in real estate are numerous. With well-chosen assets, investors can enjoy predictable cash flow, excellent returns, tax advantages, and diversification—and it's possible to leverage real estate to build wealth. Real estate investors make money through rental income, appreciation, and profits generated by business activities that depend on the property."
+  },
+  {
+    heading: "4. Invest Continuously",
+    msg: "You must invest every month Or at least at every interval in instruments that provide compounding benefit. For example, mutual funds. The fundamental idea of mutual funds is to systematically accumulate units and/or stocks that will provide returns in the future. "
+  },
+  {
+    heading: "5. Become Debt Free ASAP! ",
+    msg: "Okay, before you start investing, you need to clear off that education/car loan. Remember, full-fledged investment comes once you have repaid your debts. Thus, if you are carrying costlier loans like a personal loan or holding a credit card, try repaying them first within the due date."
+  }
+
+
+]
