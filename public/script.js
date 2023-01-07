465da24f4f2de79f157b01d40248436c5fb6705f
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-01-07 12:54:10 +0530

    Update API keys

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 fetch("./message.json")
-  .then(response => {
+  .then((response) => {
     return response.json();
   })
-  .then(data => console.log(data));
-const accountSid = ACb852a21a31dbe07cb559b69d2fe6dd9a;
-const authToken = b00e71a711a4790e3025a9667c593b01;
-const client = require('twilio')(accountSid, authToken);
+  .then((data) => console.log(data));
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
 
 client.messages
   .create({
-    from: 'whatsapp:+14155238886',
-    body: 'Hello there!',
-    to: 'whatsapp:+15005550006'
+    from: "whatsapp:+14155238886",
+    body: "Hello there!",
+    to: "whatsapp:+15005550006",
   })
-  .then(message => console.log(message.sid));
+  .then((message) => console.log(message.sid));
